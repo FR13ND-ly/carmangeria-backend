aa68cf9ef2729bbd275c9faa4a7904feb588fc99
FR13ND-ly <57838113+FR13ND-ly@users.noreply.github.com>
2024-02-11 09:52:27 +0200

    u

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from django.http.response import JsonResponse
 from .models import User, Token
-from django.contrib.auth.hashers import make_password, verify_password
+from django.contrib.auth.hashers import make_password, check_password
 import secrets
 import string
 
@@ -15,7 +15,7 @@ def authentificate(request):
     }
     if (not user.exists()): return JsonResponse(res, safe=False)
     user = user[0   ]
-    if (verify_password(data["password"], user.password)[0]):
+    if (check_password(data["password"], user.password)[0]):
         token = Token.objects.create(
             userId = user.id,
             token = ''.join(secrets.choice(string.ascii_uppercase + string.ascii_lowercase) for i in range(15))
